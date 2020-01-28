bbcd2ce14b15d7601ace751df6f0e51bc921f816
Sabbir Hossain <smac@labs-mac.local>
2020-01-28 16:12:33 +0600

    Initial Commit

---
+++ CoreDataRelationship/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CoreDataRelationship
+//
+//  Created by Sabbir Hossain on 1/28/20.
+//  Copyright © 2020 Sabbir Hossain. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
